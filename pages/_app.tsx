33237361a045fcb6004dd42900251bf84a0c5d54
Seth McCullough <seth@endevrs.dev>
2022-06-18 09:03:54 -0400

    Add plausible provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import PlausibleProvider from "next-plausible";
 import theme from "styles/theme";
 import { Navbar, Footer } from "components";
 
@@ -6,10 +7,12 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </ChakraProvider>
+    <PlausibleProvider domain="endevrs.dev">
+      <ChakraProvider theme={theme}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </ChakraProvider>
+    </PlausibleProvider>
   );
 }
