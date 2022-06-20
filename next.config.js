7eb7b5043d7724e80d140e86b863daa1140c0dde
Seth McCullough <seth@endevrs.dev>
2022-06-19 22:05:00 -0400

    Setup mdx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX({
   reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com"],
   },
-};
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
