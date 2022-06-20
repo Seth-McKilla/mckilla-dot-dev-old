f9237d67b4aa5ae5784845c0c6d6f6032bcca148
Seth McCullough <seth@endevrs.dev>
2022-06-20 06:56:12 -0400

    Add link to post cards

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -2,9 +2,8 @@ import { remark } from "remark";
 import html from "remark-html";
 import prism from "remark-prism";
 
-export default async function markdownToHtml(markdown: string) {
+export default async function markdownToHtml(markdown: any) {
   const result = await remark()
-    // https://github.com/sergioramos/remark-prism/issues/265
     .use(html, { sanitize: false })
     .use(prism)
     .process(markdown);
