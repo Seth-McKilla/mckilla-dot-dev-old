6e73e6e27bba4ee5b92b1f68c5ad5c7d392b75b7
Seth McCullough <seth@endevrs.dev>
2022-06-23 17:39:03 -0400

    Change post card to specify coming soon

--- components/CardPost.tsx
+++ components/CardPost.tsx
@@ -20,64 +20,64 @@ export default function CardPost({ slug, meta }: Post) {
 
   return (
     <Center py={6}>
-      <Link href={`/posts/${slug}`}>
-        <Box
-          as={motion.div}
-          maxW={{ base: 300, sm: 350 }}
-          w="full"
-          bg={useColorModeValue("white", "gray.900")}
-          boxShadow="2xl"
-          rounded="md"
-          p={5}
-          overflow="hidden"
-          cursor="pointer"
-          whileHover={{ scale: 1.025 }}
-          whileTap={{ scale: 0.975 }}
-        >
-          <Box h="250px" bg="gray.100" mt={-6} mx={-6} mb={6} pos="relative">
-            <Image
-              src={`/images/posts/${image}`}
-              layout="fill"
-              alt="preview-image"
-              placeholder="blur"
-              blurDataURL={`/images/posts/${image}`}
-            />
-          </Box>
-          <Stack>
-            <Text
-              color="green.500"
-              textTransform="uppercase"
-              fontWeight={800}
-              fontSize="sm"
-              letterSpacing={1.1}
-            >
-              {series}
-            </Text>
-            <Heading
-              color={useColorModeValue("gray.700", "white")}
-              fontSize="2xl"
-              fontFamily="body"
-            >
-              {title}
-            </Heading>
-            <Text color={useColorModeValue("gray.700", "gray.300")}>
-              {description}
-            </Text>
-          </Stack>
-          <Text color={"gray.400"} mb={4} mt={2} fontSize="sm">
-            {new Date(date).toLocaleDateString()} • {readTime} min read
-          </Text>
-          <Wrap>
-            {tags.split(",").map((tag, idx) => (
-              <WrapItem key={`${idx}-${tag}`}>
-                <Tag p={1} size="sm" variant="solid" colorScheme="blue">
-                  {tag}
-                </Tag>
-              </WrapItem>
-            ))}
-          </Wrap>
+      {/* <Link href={`/posts/${slug}`}> */}
+      <Box
+        as={motion.div}
+        maxW={{ base: 300, sm: 350 }}
+        w="full"
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow="2xl"
+        rounded="md"
+        p={5}
+        overflow="hidden"
+        cursor="pointer"
+        whileHover={{ scale: 1.025 }}
+        whileTap={{ scale: 0.975 }}
+      >
+        <Box h="250px" bg="gray.100" mt={-6} mx={-6} mb={6} pos="relative">
+          <Image
+            src={`/images/posts/${image}`}
+            layout="fill"
+            alt="preview-image"
+            placeholder="blur"
+            blurDataURL={`/images/posts/${image}`}
+          />
         </Box>
-      </Link>
+        <Stack>
+          <Text
+            color="green.500"
+            textTransform="uppercase"
+            fontWeight={800}
+            fontSize="sm"
+            letterSpacing={1.1}
+          >
+            {series}
+          </Text>
+          <Heading
+            color={useColorModeValue("gray.700", "white")}
+            fontSize="2xl"
+            fontFamily="body"
+          >
+            {title}
+          </Heading>
+          <Text color={useColorModeValue("gray.700", "gray.300")}>
+            {description}
+          </Text>
+        </Stack>
+        <Text color={"gray.400"} mb={4} mt={2} fontSize="sm">
+          {new Date(date).toLocaleDateString()} • {readTime} min read
+        </Text>
+        <Wrap>
+          {tags.split(",").map((tag, idx) => (
+            <WrapItem key={`${idx}-${tag}`}>
+              <Tag p={1} size="sm" variant="solid" colorScheme="blue">
+                {tag}
+              </Tag>
+            </WrapItem>
+          ))}
+        </Wrap>
+      </Box>
+      {/* </Link> */}
     </Center>
   );
 }
