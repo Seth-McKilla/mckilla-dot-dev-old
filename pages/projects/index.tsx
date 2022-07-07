f5d2eb9142ceffa3d2ddc163940a058c4b434687
Seth McCullough <seth@endevrs.dev>
2022-07-07 10:49:24 -0400

    Add projects placeholder page

---
+++ pages/projects/index.tsx
@@ -0,0 +1,36 @@
+import { Flex, Container, Heading, VStack } from "@chakra-ui/react";
+
+import { BasicLayout } from "layouts";
+import { LinkGitHub } from "components";
+
+import type { NextPage } from "next";
+
+const Projects: NextPage = () => (
+  <BasicLayout
+    meta={{
+      title: "ENDEVRS | Projects",
+      description: "A collection of my current and past projects.",
+      cardImage: "/images/seth-headshot.png",
+    }}
+  >
+    <Flex
+      p={8}
+      mt={20}
+      flex={1}
+      align="center"
+      justify="center"
+      direction="column"
+    >
+      <LinkGitHub path="Projects" />
+      <Container maxW="container.lg">
+        <VStack>
+          <Heading as="h1" size="xl">
+            Projects placeholder
+          </Heading>
+        </VStack>
+      </Container>
+    </Flex>
+  </BasicLayout>
+);
+
+export default Projects;
