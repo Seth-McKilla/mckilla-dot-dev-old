c3c1029c3aee36e4c98d703448bd3b907f746f2b
Seth McCullough <seth@endevrs.dev>
2022-07-07 10:47:31 -0400

    Add sponsors placeholder page

---
+++ pages/sponsors.tsx
@@ -0,0 +1,36 @@
+import { Flex, Container, Heading, VStack } from "@chakra-ui/react";
+
+import { BasicLayout } from "layouts";
+import { LinkGitHub } from "components";
+
+import type { NextPage } from "next";
+
+const Sponsors: NextPage = () => (
+  <BasicLayout
+    meta={{
+      title: "ENDEVRS | Sponsors",
+      description: "A thank you to my Diamond Sponsors.",
+      cardImage: "/images/seth-headshot.png",
+    }}
+  >
+    <Flex
+      p={8}
+      mt={20}
+      flex={1}
+      align="center"
+      justify="center"
+      direction="column"
+    >
+      <LinkGitHub path="sponsors" />
+      <Container maxW="container.lg">
+        <VStack>
+          <Heading as="h1" size="xl">
+            Sponsors placeholder
+          </Heading>
+        </VStack>
+      </Container>
+    </Flex>
+  </BasicLayout>
+);
+
+export default Sponsors;
