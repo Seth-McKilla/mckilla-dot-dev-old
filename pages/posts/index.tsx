4777e43066cc9f6bd93d18b56fa9e634d1db4b2c
Seth McCullough <seth@endevrs.dev>
2022-07-09 16:49:28 -0400

    Add zIndex to posts card to ensure on top

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -13,39 +13,41 @@ interface Props {
 }
 
 const Posts: NextPage<Props> = ({ posts }) => (
-  <BasicLayout
-    meta={{
-      title: "ENDEVRS | Posts",
-      description:
-        "A list of articles on various web development topics including SaaS, NextJS, and Typescript.",
-      cardImage: "/images/logo-black.png",
-    }}
-  >
-    <Flex mt={20} p={{ base: 0, sm: 8 }} flex={1} direction="column">
-      <LinkGitHub path="posts/index.tsx" />
-      <Container maxW="container.lg">
-        <Heading
-          fontSize={{ base: "3xl", sm: "5xl" }}
-          mb={{ base: 0, sm: 5 }}
-          textAlign={{
-            base: "center",
-            sm: "left",
-          }}
-        >
-          {"Featured Posts"}
-        </Heading>
-
-        <HStack justify={{ base: "center", sm: "left" }}>
-          {posts?.map((post: Post, idx) => (
-            <Fragment key={`${idx}-${post.slug}`}>
-              <CardPost {...post} />
-            </Fragment>
-          ))}
-        </HStack>
-      </Container>
-    </Flex>
+  <>
     <CardSubscribe />
-  </BasicLayout>
+    <BasicLayout
+      meta={{
+        title: "ENDEVRS | Posts",
+        description:
+          "A list of articles on various web development topics including SaaS, NextJS, and Typescript.",
+        cardImage: "/images/logo-black.png",
+      }}
+    >
+      <Flex mt={20} p={{ base: 0, sm: 8 }} flex={1} direction="column">
+        <LinkGitHub path="posts/index.tsx" />
+        <Container maxW="container.lg">
+          <Heading
+            fontSize={{ base: "3xl", sm: "5xl" }}
+            mb={{ base: 0, sm: 5 }}
+            textAlign={{
+              base: "center",
+              sm: "left",
+            }}
+          >
+            {"Featured Posts"}
+          </Heading>
+
+          <HStack justify={{ base: "center", sm: "left" }}>
+            {posts?.map((post: Post, idx) => (
+              <Fragment key={`${idx}-${post.slug}`}>
+                <CardPost {...post} />
+              </Fragment>
+            ))}
+          </HStack>
+        </Container>
+      </Flex>
+    </BasicLayout>
+  </>
 );
 
 export async function getStaticProps() {
