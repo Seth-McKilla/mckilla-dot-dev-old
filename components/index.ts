82440c63105d3562ccf46bd538009b653878c5fe
Seth McCullough <seth@endevrs.dev>
2022-07-09 08:03:04 -0400

    Add Subscribe Form component

--- components/index.ts
+++ components/index.ts
@@ -7,5 +7,6 @@ export { default as LinkIcon } from "./LinkIcon";
 export { default as LinkText } from "./LinkText";
 
 export { default as CardPost } from "./CardPost";
+export { default as CardSubscribe } from "./CardSubscribe";
 
 export { default as MDXComponents } from "./MDXComponents";
