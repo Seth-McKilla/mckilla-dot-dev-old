7588b2bc186e989f369dc039b9cd46d95c448724
Seth McCullough <seth@endevrs.dev>
2022-11-03 06:45:20 -0400

    Replace homepage with about me page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,96 +1,246 @@
 import Image from "next/image";
+import Typewriter from "typewriter-effect";
 import {
-  Container,
   Flex,
+  Container,
+  Box,
   Heading,
   Text,
-  Stack,
-  useColorMode,
+  HStack,
+  VStack,
 } from "@chakra-ui/react";
+import { FaNodeJs, FaGithub, FaTwitter } from "react-icons/fa";
+import {
+  SiTypescript,
+  SiNextdotjs,
+  SiChakraui,
+  SiMaterialui,
+  SiMongodb,
+  SiJavascript,
+  SiReact,
+  SiRedux,
+  SiTailwindcss,
+  SiPrisma,
+} from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
 import { BasicLayout } from "layouts";
-import { LinkButton, LinkGitHubSource } from "components";
+import { LinkGitHubSource, LinkIcon } from "components";
+import { shuffle } from "utils";
 
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
-  const { colorMode } = useColorMode();
+const passions = [
+  "all things javascript.",
+  "React and the NextJS framework in particular.",
+  "the Legend of Zelda series.",
+  "Software as a Service.",
+  "the occasional hackathon.",
+  "solving interesting problems with software.",
+  "talking with / teaching others about software.",
+  "collaborating with people who also have a passion for software.",
+  "entrepreneurial ventures within the tech space.",
+  "consistently learning new things.",
+  "getting my hands dirty by exploring new technology.",
+  "meditation and self-betterment.",
+];
 
-  return (
-    <BasicLayout
-      meta={{
-        title: "ENDEVRS",
-        description: "Seth McKilla's endeavors in software development",
-        cardImage: "/images/logo-black.png",
-      }}
+const AboutMe: NextPage = () => (
+  <BasicLayout
+    meta={{
+      title: "ENDEVRS | About me",
+      description: "Get to know more about Seth.",
+      cardImage: "/images/seth-headshot.png",
+    }}
+  >
+    <Flex
+      p={8}
+      mt={20}
+      flex={1}
+      align="center"
+      justify="center"
+      direction="column"
     >
-      <Flex
-        p={8}
-        flex={1}
-        align="center"
-        justify="center"
-        direction="column"
-        minH="100vh"
-      >
-        <LinkGitHubSource path="index.tsx" />
-        <Container maxW="container.lg">
-          <Stack spacing={6} w="full" maxW="lg">
-            <Heading
-              fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
-              fontWeight="medium"
-              mb={4}
-            >
-              {"Welcome to"}
-            </Heading>
+      <LinkGitHubSource path="about-me.tsx" />
+      <Container maxW="container.lg">
+        <VStack>
+          <Box
+            sx={{
+              height: "250px",
+              width: "250px",
+              backgroundColor: "blue.700",
+              borderRadius: "50%",
+              boxShadow: "0 10px 15px 0px rgba(0, 0, 0, 0.8)",
+              animation: "logo-float 5s ease-in-out infinite",
+              "@keyframes logo-float": {
+                "0%": {
+                  boxShadow: "0 10px 15px 0px rgba(0, 0, 0, 0.8)",
+                  transform: "translatey(0px)",
+                },
+                "50%": {
+                  boxShadow: "0 35px 15px 0px rgba(0, 0, 0, 0.4)",
+                  transform: "translatey(-25px)",
+                },
+                "100%": {
+                  boxShadow: "0 10px 15px 0px rgba(0, 0, 0, 0.8)",
+                  transform: "translatey(0px)",
+                },
+              },
+            }}
+            mb={5}
+          >
             <Image
-              src={`/images/logo/full-${colorMode}.png`}
-              alt="endevrs-logo"
-              placeholder="blur"
-              blurDataURL={`/images/logo/full-${colorMode}.png`}
-              width={602}
-              height={108}
+              src="/images/seth-headshot.png"
+              alt="seth-headshot"
+              width={250}
+              height={250}
             />
-            <Text
-              fontSize={{
-                base: "lg",
-                md: "xl",
-                lg: "2xl",
-              }}
-            >
-              {
-                "Follow along in my endeavors to create profitable software by developing "
-              }
-              <em>mostly</em>
-              {" bootstrapped web applications in public."}
-            </Text>
-            <Stack
-              spacing={8}
-              direction={{
-                base: "column",
-                md: "row",
-              }}
-            >
-              <LinkButton href="/projects">{"Projects"}</LinkButton>
-              <LinkButton href="/posts">{"Posts"}</LinkButton>
-            </Stack>
-          </Stack>
-        </Container>
-      </Flex>
-      <Flex flex={1} overflow="hidden" minH="100vh">
-        <Image
-          src="https://images.unsplash.com/photo-1569748130764-3fed0c102c59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2400&q=80"
-          width={2400}
-          height={1600}
-          alt="Coding IDE"
-          placeholder="blur"
-          blurDataURL="https://images.unsplash.com/photo-1569748130764-3fed0c102c59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2400&q=80"
-          objectFit="cover"
-          style={{
-            filter: "blur(2px) brightness(0.5)",
-          }}
-        />
-      </Flex>
-    </BasicLayout>
-  );
-};
+          </Box>
 
-export default Home;
+          <Heading fontSize={{ base: "xl", lg: "3xl" }} pb={4}>
+            {"Hey, I'm Seth!"}
+          </Heading>
+          <Text>
+            {"A self-taught full stack JS dev from the East Coast US."}
+          </Text>
+          <Text>{"Some things I'm passionate about..."}</Text>
+          <Typewriter
+            options={{
+              strings: shuffle(passions),
+              autoStart: true,
+              loop: true,
+              delay: 50,
+              deleteSpeed: 25,
+            }}
+          />
+
+          <Heading fontSize={{ base: "xl", lg: "3xl" }} pt={4} pb={4}>
+            {"Stack Specialties"}
+          </Heading>
+          <HStack spacing={6}>
+            <LinkIcon
+              label="Javascript"
+              href="https://www.javascript.com/"
+              size={14}
+              hoverColor="#F0DB4F"
+            >
+              <SiJavascript />
+            </LinkIcon>
+            <LinkIcon
+              label="Typescript"
+              href="https://www.typescriptlang.org/"
+              size={14}
+              hoverColor="blue.600"
+            >
+              <SiTypescript />
+            </LinkIcon>
+            <LinkIcon
+              label="React"
+              href="https://reactjs.org/"
+              size={14}
+              hoverColor="#61DAFB"
+            >
+              <SiReact />
+            </LinkIcon>
+            <LinkIcon
+              label="Redux"
+              href="https://redux.js.org/"
+              size={14}
+              hoverColor="#764ABC"
+            >
+              <SiRedux />
+            </LinkIcon>
+          </HStack>
+          <HStack spacing={6}>
+            <LinkIcon label="NextJS" href="https://nextjs.org/" size={14}>
+              <SiNextdotjs />
+            </LinkIcon>
+            <LinkIcon
+              label="Chakra UI"
+              href="https://chakra-ui.com/"
+              size={14}
+              hoverColor="teal.500"
+            >
+              <SiChakraui />
+            </LinkIcon>
+            <LinkIcon
+              label="MaterialUI"
+              href="https://mui.com/"
+              size={14}
+              hoverColor="#007FFF"
+            >
+              <SiMaterialui />
+            </LinkIcon>
+            <LinkIcon
+              label="TailwindCSS"
+              href="https://tailwindcss.com/"
+              size={14}
+              hoverColor="#38bDF8"
+            >
+              <SiTailwindcss />
+            </LinkIcon>
+          </HStack>
+          <HStack spacing={6}>
+            <LinkIcon
+              label="NodeJS"
+              href="https://nodejs.org/en/"
+              size={14}
+              hoverColor="#68A063"
+            >
+              <FaNodeJs />
+            </LinkIcon>
+            <LinkIcon
+              label="Prisma"
+              href="https://www.prisma.io/"
+              size={14}
+              hoverColor="#5A67D8"
+            >
+              <SiPrisma />
+            </LinkIcon>
+            <LinkIcon
+              label="MongoDB"
+              href="https://www.mongodb.com/"
+              size={14}
+              hoverColor="#3FA037"
+            >
+              <SiMongodb />
+            </LinkIcon>
+            <LinkIcon
+              label="MySQL"
+              href="https://www.mysql.com/"
+              size={14}
+              hoverColor="#00758F"
+            >
+              <GrMysql />
+            </LinkIcon>
+          </HStack>
+
+          <Heading fontSize={{ base: "xl", lg: "2xl" }} pt={5}>
+            {"Want to know more?"}
+          </Heading>
+          <Heading fontSize={{ base: "xl", lg: "2xl" }}>
+            {"Let's connect!"}
+          </Heading>
+          <HStack spacing={6} pt={5}>
+            <LinkIcon
+              label="Twitter"
+              href="https://twitter.com/sethmckilla"
+              size={14}
+              hoverColor="twitter.500"
+            >
+              <FaTwitter />
+            </LinkIcon>
+            <LinkIcon
+              label="GitHub"
+              href="https://github.com/seth-mckilla"
+              size={14}
+            >
+              <FaGithub />
+            </LinkIcon>
+          </HStack>
+        </VStack>
+      </Container>
+    </Flex>
+  </BasicLayout>
+);
+
+export default AboutMe;
